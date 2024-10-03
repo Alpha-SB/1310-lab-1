70bb1a2d9bc9382ed7a3e194ee0546486c2e0f1d
TonyCos5 <arcosentin42@tntech.edu>
2024-10-03 11:44:04 -0500

    forest.cpp changes

--- Forest.cpp
+++ Forest.cpp
@@ -1,122 +1,162 @@
-#include <iostream>
-#include<fstream> //used to read from and write to text files
-#include<string>
-#include<iomanip>
-#include<cctype>
-#include<climits>
+#include "Forest.h"
 
-using namespace std;
-
-class Forest 
+Forest::Forest() : currentRoom("Start Room"), gameOver(false)
 {
-    private:
-        string rooms[7] = {"Start Room", "Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Fight Room"};
-        
-        string currentRoom;
-        bool gameOver;
+    rooms[0] = "Start Room";
+    rooms[1] = "Room 1";
+    rooms[2] = "Room 2";
+    rooms[3] = "Room 3";
+    rooms[4] = "Room 4";
+    rooms[5] = "Room 5";
+    rooms[6] = "Fight Room";
 
-    public:
-        Forest() : currentRoom("Start Room"), gameOver(false) {}  // Constructor to initialize game
-        
-        ~Forest()
+    roomItems = new string**[7];
+
+    for (int i = 0; i < 7; ++i) 
+    {
+        roomItems[i] = new string*[2];
+    }
+
+    roomItems[1][0] = new string("Map");
+    roomItems[1][1] = new string("Key");
+
+    roomItems[2][0] = new string("Sword");
+    roomItems[2][1] = new string("Shield");
+
+    roomItems[4][0] = new string("Healing Potion");
+    roomItems[4][1] = new string("Armor");
+
+    roomItems[5][0] = new string("Bow");
+    roomItems[5][1] = new string("Arrows");
+}
+
+Forest::~Forest() 
+{
+    for (int i = 0; i < 7; ++i) 
+    {
+        for (int j = 0; j < 2; ++j)
         {
-
+            delete roomItems[i][j];
         }
+        delete[] roomItems[i];
+    }
+    delete[] roomItems;
+}
 
-        void displayCurrentRoom() 
+void Forest::displayCurrentRoom() 
+{
+    cout << "\nYou are currently in " << currentRoom << endl;
+    displayRoomItems();
+}
+
+void Forest::displayRoomItems() 
+{
+    int roomIndex = getRoomIndex(currentRoom);
+    if (roomIndex != -1) 
+    {
+        cout << "Items in this room: " << *roomItems[roomIndex][0] << ", " << *roomItems[roomIndex][1] << endl;
+    }
+}
+
+int Forest::getRoomIndex(const string& room) 
+{
+    for (int i = 0; i < 7; ++i) 
+    {
+        if (rooms[i] == room) 
         {
-            cout << "\nYou are currently in " << currentRoom << endl;
+            return i;
         }
+    }
+    return -1;  // Room not found
+}
 
-        void startGame() 
+void Forest::startGame() 
+{
+    while (!gameOver) 
+    {
+        if (currentRoom == "Start Room") 
         {
-            while (!gameOver) 
-            {
-                if (currentRoom == "Start Room") 
-                {
-                    handleStartLine();
-                } 
-                else if (currentRoom == "Room 1" || currentRoom == "Room 2") 
-                {
-                    handleRoom1and2();
-                } 
-                else if (currentRoom == "Room 3") 
-                {
-                    handleRoom3();
-                } 
-                else if (currentRoom == "Room 4" || currentRoom == "Room 5") 
-                {
-                    handleRoom4and5();
-                } 
-                else if (currentRoom == "Fight Room") 
-                {
-                    handleFightRoom();
-                }
-            }
+            handleStartLine();
+        } 
+        else if (currentRoom == "Room 1" || currentRoom == "Room 2") 
+        {
+            handleRoom1and2();
+        } 
+        else if (currentRoom == "Room 3") 
+        {
+            handleRoom3();
+        } 
+        else if (currentRoom == "Room 4" || currentRoom == "Room 5") 
+        {
+            handleRoom4and5();
+        } 
+        else if (currentRoom == "Fight Room") 
+        {
+            handleFightRoom();
         }
+    }
+}
 
-    private:
-        void handleStartLine() 
-        {
-            int input;
-            do
-            {
-                displayCurrentRoom();
-                cout << "What would you like to do?" << endl;
-                cout << "1: to Go to Room 1" << endl;
-                cout << "2: to Go to Room 2" << endl;
-                cin >> input;
-            }
-            while (input != 1 && input != 2);
-            currentRoom = rooms[input];
-        }
+void handleStartLine() 
+{
+    int input;
+    do
+    {
+        displayCurrentRoom();
+        cout << "What would you like to do?" << endl;
+        cout << "1: to Go to Room 1" << endl;
+        cout << "2: to Go to Room 2" << endl;
+        cin >> input;
+    }
+    while (input != 1 && input != 2);
+    currentRoom = rooms[input];
+}
 
-        void handleRoom1and2() 
-        {
-            int input;
-            do
-            {
-                displayCurrentRoom();
-                cout << "What would you like to do?" << endl;
-                cout << "3: to Go to Room 3" << endl;
-                cin >> input;
-            }
-            while (input != 3);
-            currentRoom = rooms[input];
-        }
+void handleRoom1and2() 
+{
+    int input;
+    do
+    {
+        displayCurrentRoom();
+        cout << "What would you like to do?" << endl;
+        cout << "3: to Go to Room 3" << endl;
+        cin >> input;
+    }
+    while (input != 3);
+    currentRoom = rooms[input];
+}
 
-        void handleRoom3() 
-        {
-            int input;
-            do
-            {
-                displayCurrentRoom();
-                cout << "What would you like to do?" << endl;
-                cout << "4: to Go to Room 4" << endl;
-                cout << "5: to Go to Room 5" << endl;
-                cin >> input;
-            }
-            while (input != 4 && input != 5);
-            currentRoom = rooms[input];
-        }
+void handleRoom3() 
+{
+    int input;
+    do
+    {
+        displayCurrentRoom();
+        cout << "What would you like to do?" << endl;
+        cout << "4: to Go to Room 4" << endl;
+        cout << "5: to Go to Room 5" << endl;
+        cin >> input;
+    }
+    while (input != 4 && input != 5);
+    currentRoom = rooms[input];
+}
 
-        void handleRoom4and5() 
-        {
-            int input;
-            do
-            {
-                displayCurrentRoom();
-                cout << "Enter 6 to go to the Boss Fight" << endl;
-                cin >> input;
-            }
-            while (input != 6);
-            currentRoom = "Fight Room";
-        }
+void handleRoom4and5() 
+{
+    int input;
+    do
+    {
+        displayCurrentRoom();
+        cout << "Enter 6 to go to the Boss Fight" << endl;
+        cin >> input;
+    }
+    while (input != 6);
+    currentRoom = "Fight Room";
+}
 
-        void handleFightRoom() 
-        {
-            displayCurrentRoom();
-            cout << "You have reached the end of the game!" << endl;
-            gameOver = true;
-        }
-};
+void Forest::handleFightRoom() 
+{
+    displayCurrentRoom();
+    cout << "You have reached the end of the game!" << endl;
+    gameOver = true;
+}
