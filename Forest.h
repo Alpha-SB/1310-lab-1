3527483f599c777134010852e9f39f9da9693e6a
TonyCos5 <arcosentin42@tntech.edu>
2024-10-03 11:47:22 -0500

    Forest.h add

--- Forest.h
+++ Forest.h
@@ -9,27 +9,32 @@
 #include <climits>
 
 using namespace std;
-
-class Forest
+class Forest 
 {
     private:
-        string rooms[7] = {"Start Room", "Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Fight Room"};
-        string currentRoom;
-        bool gameOver;
+        string rooms[7];        // Room names
+        string currentRoom;      // Current room of the player
+        string*** roomItems;     // Pointer to an array of pointers for items in the rooms
+        bool gameOver;           // Game state flag
 
+    public:
+        // Constructor and Destructor
+        Forest();
+        ~Forest();
+
+        // Public Methods
+        void displayCurrentRoom();
+        void startGame();
+
+    private:
+        // Private Helper Methods
+        void displayRoomItems();
+        int getRoomIndex(const string& room);
         void handleStartLine();
         void handleRoom1and2();
         void handleRoom3();
         void handleRoom4and5();
         void handleFightRoom();
-    
-    public:
-    Forest();
-    ~Forest();
-
-    void displayCurrentRoom();
-    void startGame();
-
 };
 
 #endif
